bf0f8f9ca22c8dd1803150be43cadce747147ad1
Sukrit Jaiswal <83272846+sukritjaiswal@users.noreply.github.com>
2024-07-05 14:30:47 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+    <App />
+    </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
